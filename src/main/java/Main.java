6e6b60960579a0523d168de44c44c7ff3a4f922d
Cesar <cesar.kina@gmail.com>
2020-11-18 21:09:37 -0500

    Create ReportService class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,114 +1,25 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.api.client.util.Base64;
-import com.google.api.services.gmail.model.ListMessagesResponse;
 import com.google.api.services.gmail.model.Message;
-import com.google.api.services.sheets.v4.model.ValueRange;
-import io.github.cdimascio.dotenv.Dotenv;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import us.codecraft.xsoup.Xsoup;
 
 import java.io.*;
-import java.security.GeneralSecurityException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
-    private static final Dotenv dotenv = Dotenv.load();
-    private static final String spreadsheetId = dotenv.get("GOOGLE_SHEET_ID");
-    private static final String sheetAllRange = "Main!A1:J";
-    private static final String sheetInternalDateRange = "Main!A1:A";
-    private static final String dateQueryExpression = "<DATE_QUERY>";
 
-    public static void main(String... args) throws IOException, GeneralSecurityException {
-        // Build a new authorized API client service.
-        Long lastInternalDate = null;
-        ValueRange internalDates = GoogleClient.SHEETS_CLIENT.spreadsheets().values().get(spreadsheetId, sheetInternalDateRange).execute();
-        if(internalDates.getValues() != null) {
-            List<Object> lastRow = internalDates.getValues().get(internalDates.getValues().size() - 1);
-            lastInternalDate = Long.parseLong(lastRow.get(0).toString());
-        }
-
-        // Read configuration file
-        ObjectMapper objectMapper = new ObjectMapper();
-        InputStream in = Main.class.getResourceAsStream("/config.json");
-        Map<String, Object> mapObject = objectMapper.readValue(in, Map.class);
-        String query = mapObject.get("query").toString();
-        if (lastInternalDate != null) {
-            query = query.replaceAll(dateQueryExpression, " AND after:" + Main.dateToString(new Date(lastInternalDate)));
-        } else {
-            query = query.replaceAll(dateQueryExpression, "");
-        }
-
-        List<Map<String, String>> fields = (List<Map<String, String>>) mapObject.get("fields");
+    public static void main(String... args) throws IOException {
+        Long lastInternalDate = ReportService.getLastInternalDate();
+        String query = ReportService.getQuery(lastInternalDate);
+        List<Map<String, String>> fields = (List<Map<String, String>>) ReportService.CONFIG.get("fields");
 
         // Search operators in Gmail: https://support.google.com/mail/answer/7190?hl=en
-        ListMessagesResponse listMessages = GoogleClient.GMAIL_CLIENT.users().messages().list("me").setQ(query).execute();
-        List<Message> messages = listMessages.getMessages();
+        List<Message> messages = GoogleClient.GMAIL_CLIENT.users().messages().list("me").setQ(query).execute().getMessages();
 
         if (messages.isEmpty()) {
             System.out.println("No messages found.");
         } else {
-            List<List<Object>> newValues = new ArrayList<>();
-
-            for (Message message : messages) {
-                Message fullMessage = GoogleClient.GMAIL_CLIENT.users().messages().get("me", message.getId()).setFormat("full").execute();
-
-                if(lastInternalDate != null && fullMessage.getInternalDate() > lastInternalDate){
-                    System.out.println("Processing message: " + message.getId());
-                    String body = new String(Base64.decodeBase64(fullMessage.getPayload().getBody().getData().getBytes()));
-
-                    Document document = Jsoup.parse(body);
-
-                    List<Object> cellValues = new ArrayList<>();
-                    cellValues.add(fullMessage.getInternalDate());
-
-                    for(Map<String, String> field: fields) {
-                        String value = Main.readDocument(document, field.get("xpath"));
-                        if(field.get("regex") != null) {
-                            Pattern compile = Pattern.compile(field.get("regex"));
-                            Matcher matcher = compile.matcher(value);
-                            if(matcher.find()) {
-                                value = matcher.group("matcherGroup");
-                            }
-                        }
-                        cellValues.add(value);
-                    }
-
-                    newValues.add(cellValues);
-                } else {
-                    System.out.println("Skipping message: " + message.getId());
-                }
-            }
-
-            Collections.sort(newValues, Comparator.comparingLong(value -> (long) value.get(0)));
-
-            ValueRange valueRange = new ValueRange().setValues(newValues);
-            GoogleClient.SHEETS_CLIENT.spreadsheets().values().append(spreadsheetId, sheetAllRange, valueRange)
-                    .setValueInputOption("USER_ENTERED")
-                    .execute();
+            List<List<Object>> newValues = ReportService.calculateNewValues(messages, lastInternalDate, fields);
+            ReportService.appendRowsToSheets(newValues);
             System.out.println("Done.");
         }
     }
 
-    public static String readDocument(Document doc, String xpath) {
-        List<Element> elements = Xsoup.compile(xpath).evaluate(doc).getElements();
-        for(Element e: elements) {
-            if("img".equals(e.tagName()) && xpath.endsWith("@src")) {
-                return e.attributes().get("src");
-            } else {
-                return e.text();
-            }
-        }
-        return null;
-    }
-
-    public static String dateToString(Date date) {
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
-        return dateFormat.format(date);
-    }
 }
---
+++ src/main/java/ReportService.java
@@ -0,0 +1,120 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.util.Base64;
+import com.google.api.services.gmail.model.Message;
+import com.google.api.services.sheets.v4.model.ValueRange;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import us.codecraft.xsoup.Xsoup;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ReportService {
+
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String spreadsheetId = dotenv.get("GOOGLE_SHEET_ID");
+    private static final String sheetAllRange = "Main!A1:J";
+    private static final String sheetInternalDateRange = "Main!A1:A";
+    private static final String dateQueryExpression = "<DATE_QUERY>";
+    private static final String configFile = "/config.json";
+    public static Map<String, Object> CONFIG;
+
+    static {
+        ObjectMapper objectMapper = new ObjectMapper();
+        InputStream in = Main.class.getResourceAsStream(configFile);
+        try {
+            CONFIG = objectMapper.readValue(in, Map.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Long getLastInternalDate() throws IOException {
+        Long lastInternalDate = null;
+        ValueRange internalDates = GoogleClient.SHEETS_CLIENT.spreadsheets().values().get(spreadsheetId, sheetInternalDateRange).execute();
+        if(internalDates.getValues() != null) {
+            List<Object> lastRow = internalDates.getValues().get(internalDates.getValues().size() - 1);
+            lastInternalDate = Long.parseLong(lastRow.get(0).toString());
+        }
+        return lastInternalDate;
+    }
+
+    public static String getQuery(Long lastInternalDate) {
+        String query = ReportService.CONFIG.get("query").toString();
+        if (lastInternalDate != null) {
+            query = query.replaceAll(dateQueryExpression, " AND after:" + ReportService.dateToString(new Date(lastInternalDate)));
+        } else {
+            query = query.replaceAll(dateQueryExpression, "");
+        }
+        return query;
+    }
+
+    public static void appendRowsToSheets(List<List<Object>> newValues) throws IOException {
+        ValueRange valueRange = new ValueRange().setValues(newValues);
+        GoogleClient.SHEETS_CLIENT.spreadsheets().values().append(spreadsheetId, sheetAllRange, valueRange)
+                .setValueInputOption("USER_ENTERED")
+                .execute();
+    }
+
+    public static List<List<Object>> calculateNewValues(List<Message> messages, Long lastInternalDate, List<Map<String, String>> fields) throws IOException {
+        List<List<Object>> newValues = new ArrayList<>();
+
+        for (Message message : messages) {
+            Message fullMessage = GoogleClient.GMAIL_CLIENT.users().messages().get("me", message.getId()).setFormat("full").execute();
+
+            if(lastInternalDate != null && fullMessage.getInternalDate() > lastInternalDate){
+                System.out.println("Processing message: " + message.getId());
+                String body = new String(Base64.decodeBase64(fullMessage.getPayload().getBody().getData().getBytes()));
+
+                Document document = Jsoup.parse(body);
+
+                List<Object> cellValues = new ArrayList<>();
+                cellValues.add(fullMessage.getInternalDate());
+
+                for(Map<String, String> field: fields) {
+                    String value = ReportService.readDocument(document, field.get("xpath"));
+                    if(field.get("regex") != null) {
+                        Pattern compile = Pattern.compile(field.get("regex"));
+                        Matcher matcher = compile.matcher(value);
+                        if(matcher.find()) {
+                            value = matcher.group("matcherGroup");
+                        }
+                    }
+                    cellValues.add(value);
+                }
+
+                newValues.add(cellValues);
+            } else {
+                System.out.println("Skipping message: " + message.getId());
+            }
+        }
+
+        Collections.sort(newValues, Comparator.comparingLong(value -> (long) value.get(0)));
+        return newValues;
+    }
+
+    public static String readDocument(Document doc, String xpath) {
+        List<Element> elements = Xsoup.compile(xpath).evaluate(doc).getElements();
+        for(Element e: elements) {
+            if("img".equals(e.tagName()) && xpath.endsWith("@src")) {
+                return e.attributes().get("src");
+            } else {
+                return e.text();
+            }
+        }
+        return null;
+    }
+
+    public static String dateToString(Date date) {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        return dateFormat.format(date);
+    }
+
+}
