e23a675295cc67fcf98964426fc4c2eb91ed3b07
Cesar <cesar.kina@gmail.com>
2020-12-02 21:30:57 -0500

    Remove unnecessary imports

--- src/test/java/com/ckinan/ApplicationTests.java
+++ src/test/java/com/ckinan/ApplicationTests.java
@@ -1,14 +1,9 @@
 package com.ckinan;
 
-import com.ckinan.core.GmailProvider;
-import com.ckinan.core.GoogleSheetsReportWriter;
-import com.ckinan.core.IEmailProvider;
-import com.ckinan.core.IReportWriter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
-import java.util.List;
 
 @SpringBootTest
 class ApplicationTests {
